b299ad2302ffd5b9315c0919ada02ebd4ffffe4c
loginovw8 <loginovw8@gmail.com>
2022-07-25 19:31:45 +0300

    Lane positioning stage added.

---
+++ 12-lane-position/main.js
@@ -0,0 +1,19 @@
+const canvas = document.querySelector('canvas');
+canvas.width = 200;
+
+const ctx = canvas.getContext("2d");
+const road = new Road(canvas.width / 2, canvas.width * 0.9);
+const car = new Car(road.getLaneCenter(1), 100, 30, 50);
+
+animate();
+
+function animate() {
+    car.move();
+
+    canvas.height = window.innerHeight;
+
+    road.draw(ctx);
+    car.draw(ctx);
+
+    requestAnimationFrame(animate);
+}
