db22f79ad9c3aa119dfd09588875f1e962e2b07f
loginovw8 <loginovw8@gmail.com>
2022-09-12 23:29:05 +0300

    Lesson 2 changed.

--- 02-car/main.js
+++ 02-car/main.js
@@ -1,15 +1,16 @@
-const canvas = document.querySelector('canvas');
-canvas.width = 200;
+// const canvas = document.querySelector('canvas');
+// canvas.width = 200;
 
-const ctx = canvas.getContext("2d");
+// const ctx = canvas.getContext("2d");
+
 const car = new Car(100, 100, 30, 50);
 
-animate();
+// animate();
 
-function animate() {
-    canvas.height = window.innerHeight;
+// function animate() {
+//     canvas.height = window.innerHeight;
 
     car.draw(ctx);
 
-    requestAnimationFrame(animate);
-}
+//     requestAnimationFrame(animate);
+// }
