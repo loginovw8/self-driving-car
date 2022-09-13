037444c3ad9026ac6410b13a10a6b822095f6964
loginovw8 <loginovw8@gmail.com>
2022-09-13 08:21:04 +0300

    Lesson 3 edited.

--- 03-controls/car.js
+++ 03-controls/car.js
@@ -1,12 +1,12 @@
-class Car {
-  constructor(x, y, width, height) {
-    this.x = x;
-    this.y = y;
-    this.width = width;
-    this.height = height;
+// class Car {
+//   constructor(x, y, width, height) {
+//     this.x = x;
+//     this.y = y;
+//     this.width = width;
+//     this.height = height;
 
     this.controls = new Controls();
-  }
+  // }
 
   move() {
     if (this.controls.forward) {
@@ -19,15 +19,13 @@ class Car {
     // left and right? 
   }
 
-  draw(ctx) {
-    ctx.translate(this.x, this.y);
-    ctx.beginPath();
-    ctx.rect(
-      -this.width / 2,
-      -this.height / 2,
-      this.width,
-      this.height
-    );
-    ctx.fill();
-  }
-}
+//   draw(ctx) {
+//     ctx.rect(
+//       -this.width / 2,
+//       -this.height / 2,
+//       this.width,
+//       this.height
+//     );
+//     ctx.fill();
+//   }
+// }
--- 03-controls/main.js
+++ 03-controls/main.js
@@ -1,17 +1,17 @@
-const canvas = document.querySelector('canvas');
-canvas.width = 200;
+// const canvas = document.querySelector('canvas');
+// canvas.width = 200;
 
-const ctx = canvas.getContext("2d");
-const car = new Car(100, 100, 30, 50);
+// const ctx = canvas.getContext("2d");
+// const car = new Car(100, 100, 30, 50);
 
-animate();
+// animate();
 
-function animate() {
+// function animate() {
     car.move();
 
-    canvas.height = window.innerHeight;
+//     canvas.height = window.innerHeight;
 
-    car.draw(ctx);
+//     car.draw(ctx);
 
-    requestAnimationFrame(animate);
-}
+//     requestAnimationFrame(animate);
+// }
