b275a0048eda963720e7ccee893cac73b116b4a5
loginovw8 <loginovw8@gmail.com>
2022-09-13 15:46:18 +0300

    Lessons 5, 6, 7 edited.

--- 05-accurate/car.js
+++ 05-accurate/car.js
@@ -1,24 +1,24 @@
-class Car {
-  constructor(x, y, width, height) {
-    this.x = x;
-    this.y = y;
-    this.width = width;
-    this.height = height;
+// class Car {
+//   constructor(x, y, width, height) {
+//     this.x = x;
+//     this.y = y;
+//     this.width = width;
+//     this.height = height;
 
-    this.speed = 0;
-    this.acceleration = 0.2;
+//     this.speed = 0;
+//     this.acceleration = 0.2;
     this.maxSpeed = 3;
 
-    this.controls = new Controls();
-  }
+  //   this.controls = new Controls();
+  // }
 
-  move() {
-    if (this.controls.forward) {
-      this.speed += this.acceleration;
-    }
-    if (this.controls.reverse) {
-      this.speed -= this.acceleration;
-    }
+  // move() {
+  //   if (this.controls.forward) {
+  //     this.speed += this.acceleration;
+  //   }
+  //   if (this.controls.reverse) {
+  //     this.speed -= this.acceleration;
+  //   }
 
     if (this.speed > this.maxSpeed) {
       this.speed = this.maxSpeed;
@@ -27,25 +27,23 @@ class Car {
       this.speed = -this.maxSpeed / 2;
     }
 
-    if (this.controls.left) {
-      this.x -= 2;
-    }
-    if (this.controls.right) {
-      this.x += 2;
-    }
+  //   if (this.controls.left) {
+  //     this.x -= 2;
+  //   }
+  //   if (this.controls.right) {
+  //     this.x += 2;
+  //   }
 
-    this.y -= this.speed;
-  }
+  //   this.y -= this.speed;
+  // }
 
-  draw(ctx) {
-    ctx.translate(this.x, this.y);
-    ctx.beginPath();
-    ctx.rect(
-      -this.width / 2,
-      -this.height / 2,
-      this.width,
-      this.height
-    );
-    ctx.fill();
-  }
-}
+//   draw(ctx) {
+//     ctx.rect(
+//       -this.width / 2,
+//       -this.height / 2,
+//       this.width,
+//       this.height
+//     );
+//     ctx.fill();
+//   }
+// }
